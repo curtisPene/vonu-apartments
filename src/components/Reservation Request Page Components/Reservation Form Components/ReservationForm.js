c49566651ba42e4821abd7324c06e0b609ebb9f2
Curtis <curtispene92@gmail.com>
2023-10-05 17:14:18 +1200

    Add policy check box to reservation form

--- src/components/Reservation Request Page Components/Reservation Form Components/ReservationForm.js
+++ src/components/Reservation Request Page Components/Reservation Form Components/ReservationForm.js
@@ -1,20 +1,40 @@
 import classes from "./ReservationForm.module.css";
 import "../../../index.css";
 import { Form, useNavigation } from "react-router-dom";
+import { useState } from "react";
 
 const ReservationForm = ({ children }) => {
   const navigation = useNavigation();
+  const [hasAgreed, setHasAgreed] = useState(false);
+
+  const checkBoxChangeHandler = () => {
+    setHasAgreed((hasAgreed) => !hasAgreed);
+  };
 
   return (
     <Form className={classes["reservation-form"]} method="POST">
       {children}
+      <div className={`${classes.agree} drop-shadow`}>
+        <input
+          type="checkbox"
+          name="agreeToPolicies"
+          onChange={checkBoxChangeHandler}
+        />
+        <label htmlFor="">
+          I have read and agree to the policies set forth by the management of
+          Vonu Apartments
+        </label>
+      </div>
+
       <input
         type="submit"
         value={
           navigation.state === "submitting" ? "Sending your details" : "Submit"
         }
         className={`${classes.submit} drop-shadow`}
-        disabled={navigation.state === "submitting" ? true : false}
+        disabled={
+          navigation.state === "submitting" || !hasAgreed ? true : false
+        }
       />
     </Form>
   );
